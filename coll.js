e550a630c869ea2d7508e35701818781620d9cf2
fred <fredrik.sandq@gmail.com>
2022-05-08 12:48:52 +0200

    rng stuff

---
+++ coll.js
@@ -0,0 +1,11 @@
+for (let coll of document.getElementsByClassName("collapsible")) {
+  coll.addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
